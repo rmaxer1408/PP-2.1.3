dcc05649ed38fcd7999a9066febd0ddf88277fd2
Roman Osipov <rmnosipov59@gmail.com>
2023-10-06 10:51:50 +0300

    feat: add task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1 == catBean2);
     }
 }
